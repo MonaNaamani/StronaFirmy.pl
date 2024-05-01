e90b2f8f44ebb58352c46f9fa0991f81aa5261e9
stronafirmy <167859991+stronafirmy@users.noreply.github.com>
2024-05-01 23:14:05 +0200

    Integrate Storyblok for blogging

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,28 @@
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
-// https://astro.build/config
-export default defineConfig({});
+const env = loadEnv('', process.cwd(), 'STORYBLOK')
+
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        blogPost: 'storyblok/BlogPost',
+        blogPostList: 'storyblok/BlogPostList',
+        page: 'storyblok/Page',
+      },
+      apiOptions: {
+        region: 'eu',
+      },
+    }),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+})
