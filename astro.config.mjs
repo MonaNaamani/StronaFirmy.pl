c5b9d8d4e004dc2d9228fe0e2c5de77f4288ca05
stronafirmy <167859991+stronafirmy@users.noreply.github.com>
2024-05-05 17:20:53 +0200

    Use SVG icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,27 @@
 import { defineConfig } from 'astro/config';
-import storyblok from '@storyblok/astro'
-import { loadEnv } from 'vite'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import icon from "astro-icon";
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
 
-const env = loadEnv('', process.cwd(), 'STORYBLOK')
-
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        page: 'storyblok/Page',
-        'all-articles': 'storyblok/AllArticles',
-        article: 'storyblok/Article',
-      },
-      apiOptions: {
-        region: 'eu',
-      },
-    }),
-  ],
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      page: 'storyblok/Page',
+      'all-articles': 'storyblok/AllArticles',
+      article: 'storyblok/Article'
+    },
+    apiOptions: {
+      region: 'eu'
+    }
+  }), icon()],
   vite: {
     plugins: [basicSsl()],
     server: {
-      https: true,
-    },
-  },
-})
+      https: true
+    }
+  }
+});
