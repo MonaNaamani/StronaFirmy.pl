7dda3698b3f65cf49ea6cbd0b7ddf6101ee71918
stronafirmy <167859991+stronafirmy@users.noreply.github.com>
2024-05-22 14:43:58 +0200

    Remove Storyblok integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,5 @@
-import { defineConfig } from 'astro/config';
-import storyblok from '@storyblok/astro';
-import { loadEnv } from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
-import icon from "astro-icon";
-const env = loadEnv('', process.cwd(), 'STORYBLOK');
+import { defineConfig } from 'astro/config'
+import icon from 'astro-icon'
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [storyblok({
-    accessToken: env.STORYBLOK_TOKEN,
-    components: {
-      page: 'storyblok/Page',
-      'all-articles': 'storyblok/AllArticles',
-      article: 'storyblok/Article'
-    },
-    apiOptions: {
-      region: 'eu'
-    }
-  }), icon()],
-  vite: {
-    plugins: [basicSsl()],
-    server: {
-      https: true
-    }
-  }
-});
+export default defineConfig({ integrations: [icon()] })
