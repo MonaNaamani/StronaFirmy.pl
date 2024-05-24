b7ba9a4b32f02ec1f82a55cedf390de0e71c0c37
stronafirmy <167859991+stronafirmy@users.noreply.github.com>
2024-05-24 23:48:05 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config'
-import icon from 'astro-icon'
+import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://stronafirmy.pl',
-  integrations: [icon()],
-})
+  integrations: [icon(), sitemap()]
+});
